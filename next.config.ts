64f26b4a4325fdd085d42bcb32b56efd41c73f92
Ved Mahajan <mahajanved@outlook.com>
2026-07-26 19:11:05 -0700

    Initial portfolio setup

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/my-portfolio",
+};
+
+export default nextConfig;
